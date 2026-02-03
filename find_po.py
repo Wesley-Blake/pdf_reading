226fed74c1ff2f2553751856533ac6220c5d16dc
wesblake@proton.me <wesblake@proton.me>
2026-02-02 16:02:10 -0800

    Added secrets to path. updated find_po to import excel tracker.

--- find_po.py
+++ find_po.py
@@ -1,12 +1,23 @@
 import os
+from pathlib import Path
 import pdfplumber
+import pandas as pd
 
-DIR = 'some place'
-
+def get_path():
+    directory = Path().cwd() / "pdf_reading"
+    path = Path()
+    for i in directory.iterdir():
+        if i.name.endswith(".txt"):
+            path = i
+    with open(path) as f:
+        return f.readline()
 
 def pdf_search(dirpath, file, find_me):
     all_text = ""
     path = os.path.join(dirpath, file)
+    if not os.path.isfile(path):
+        print(f"Failed to find file (check path): {path}")
+        SystemExit(1)
     with pdfplumber.open(path) as pdf:
         for page in pdf.pages:
             all_text += page.extract_text() + '\n'
@@ -15,10 +26,14 @@ def pdf_search(dirpath, file, find_me):
 
 
 def main():
-    path = input("Path of interest: ")
-    if not os.path.isfile(path): SystemExit(1)
-    for dirpath, dirnames, filenames in os.walk(path):
-        pass
+    # get path from temp tracker.
+    path = get_path()
+    pd.read_excel(path)
+    print(pd)
+    #dataframe = pd.read_excel(path)
+    #if not os.path.isfile(path): SystemExit(1)
+    #for dirpath, dirnames, filenames in os.walk(path):
+    #    pass
 
 
 if __name__ == '__main__':
